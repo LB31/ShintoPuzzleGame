cb3f50a03f68fce6ac1a6ac785035ff39277b92a
Leonid <leonidb93@gmail.com>
2020-05-31 03:02:16 +0200

    switch worlds by activating/deactivating areas and teleporting player between them

---
+++ Assets/_Scripts/WorldSwitcher.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldSwitcher : MonoBehaviour
+{
+    public GameObject MainScene;
+    public GameObject SecondScene;
+    public Transform Player;
+    public Transform SpawnPoint;
+
+    private Vector3 lastPlayerPos;
+    private Quaternion lastPlayerRotation;
+
+    private bool mainWorld = true;
+
+    //private void Start()
+    //{
+    //    DontDestroyOnLoad(this);
+    //}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchWorld();
+        }
+    }
+
+    void SwitchWorld()
+    {
+        if (mainWorld)
+        {
+
+            mainWorld = !mainWorld;
+            lastPlayerPos = Player.position;
+            lastPlayerRotation = Player.rotation;
+            SecondScene.SetActive(true);
+            MainScene.SetActive(false);
+            Player.GetComponent<CharacterController>().enabled = false;
+            Player.position = SpawnPoint.position;
+            Player.rotation = SpawnPoint.rotation;
+            Player.GetComponent<CharacterController>().enabled = true;
+
+        }
+        else
+        {
+            mainWorld = !mainWorld;
+            SecondScene.SetActive(false);
+            MainScene.SetActive(true);
+            Player.GetComponent<CharacterController>().enabled = false;
+            Player.position = lastPlayerPos;
+            Player.rotation = lastPlayerRotation;
+            Player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
