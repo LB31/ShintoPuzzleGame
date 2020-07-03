87c3ed6eff3b8cbf773877e248a4c8ce1f0d385b
Katrina Rizakova <HackerKat@users.noreply.github.com>
2020-07-04 01:52:23 +0200

    on Tori trigger added

---
+++ Assets/_Scripts/DreamworlTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamworlTrigger : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayMakerFSM.BroadcastEvent("ENTER DREAMWORLD");
+    }
+}
