bb67ea844ca48451a093fa8b57cedaf14ae6be09
Katrina Rizakova <HackerKat@users.noreply.github.com>
2020-07-22 17:33:44 +0200

    added hover scriptto mask

---
+++ Assets/MaskHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskHover : MonoBehaviour
+{
+    [SerializeField]
+    private float hoverSpeed = 10.0f;
+    [SerializeField]
+    private float maxHeight = 5;
+    [SerializeField]
+    private float minHeight;
+
+    private float hoverHeight;
+    private float hoverRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoverHeight = (maxHeight + minHeight) / 2.0f;
+        hoverRange = maxHeight - minHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            this.transform.position = Vector3.up * (hoverHeight + Mathf.Cos(Time.time * hoverSpeed) * hoverRange);
+    }
+}
