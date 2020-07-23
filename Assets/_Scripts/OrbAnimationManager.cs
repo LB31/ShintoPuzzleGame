fde1c21ed7feb0955b9f0ed916a650b066937705
Katrina Rizakova <HackerKat@users.noreply.github.com>
2020-07-23 03:57:58 +0200

    animation added to each tori

--- Assets/_Scripts/OrbAnimationManager.cs
+++ Assets/_Scripts/OrbAnimationManager.cs
@@ -4,39 +4,38 @@ using UnityEngine;
 
 public class OrbAnimationManager : MonoBehaviour
 {
-
     public Animator animator;
     public GameObject player;
-<<<<<<< HEAD
-    public GameObject blockadeWall;
 
+    public int yokaiDefeated;
+
+    //public bool triggerOne = false;
+    //public bool triggertwo = false;
+    // Start is called before the first frame update
+
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
-            animator.SetBool("startAnim1", true);
-            blockadeWall.SetActive(false);
-        }
-=======
-    public bool triggerOne = false;
-    public bool triggertwo = false;
-    // Start is called before the first frame update
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == player && triggerOne)
-        {
             Debug.Log("AnimatorStart");
             animator.SetBool("startAnim1", true);
+
         }
+        ChangePaths(yokaiDefeated);
+        /*
         else if (other.gameObject == player && triggertwo)
         {
             animator.SetBool("startAnim2", true);
         }
+        */
         //animator.SetBool("startAnim1", false);
         //animator.SetBool("startAnim2", false);
->>>>>>> master
+
     }
 
-
+    public void ChangePaths(int yokaiDefeated)
+    {
+        animator.SetInteger("yokaiDefeated", yokaiDefeated);
+    }
 }
