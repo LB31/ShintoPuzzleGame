774da0a43222e228d3a502d9ecebd19a744cbee1
Leonid <leonidb93@gmail.com>
2020-07-23 03:39:23 +0200

    add game audios, implement audio script

---
+++ Assets/_Scripts/AudioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AudioController : MonoBehaviour
+{
+    public List<DictAudio> AllAudios;
+    private AudioSource AudioSource;
+    
+
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChangeAudio(string audioName)
+    {
+        AudioClip selectedClip = AllAudios.Where(entry => entry.Name == audioName).FirstOrDefault().Audio;
+        AudioSource.clip = selectedClip;
+        AudioSource.Play();
+    }
+}
+
+[Serializable]
+public class DictAudio
+{
+    public string Name;
+    public AudioClip Audio;
+}
